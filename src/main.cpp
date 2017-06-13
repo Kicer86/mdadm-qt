354ed291d55b73288b22286aeba993d8a089d6e8
Michał Walenciak <Kicer86@gmail.com>
2017-06-13 22:36:20 +0200

    introducing MainWindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 
 #include <QApplication>
 
+#include "main_window.hpp"
+
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+
+    MainWindow main_window;
+    main_window.show();
 
     return app.exec();
 }
