fe3b09b403b991c08c4871cebe68bba05648d029
Michał Walenciak <Kicer86@gmail.com>
2017-06-22 20:58:15 +0200

    adding test for MDAdmController to validate raid creation arguments

--- src/mdadm_controller_tests.cpp
+++ src/mdadm_controller_tests.cpp
@@ -1,11 +1,40 @@
 
+#include <QDebug>
+
 #include <gmock/gmock.h>
 
 #include "mdadm_controller.hpp"
+#include "imdadm_process_mock.hpp"
+
+using testing::_;
+using testing::Return;
+
+void PrintTo(const QStringList& string_list, std::ostream* os)
+{
+    QString output;
+    QDebug debug(&output);
+    debug << string_list;
+
+    *os << output.toStdString();
+}
+
 
 TEST(MDAdmControllerTest, isConstructible)
 {
     EXPECT_NO_THROW({
         MDAdmController controller(nullptr);
     });
+}
+
+
+TEST(MDAdmControllerTest, usesRightParametersForRaid0Creation)
+{
+    IMDAdmProcessMock mdadm_process;
+
+    const QStringList expected_args = { "--create", "--verbose", "/dev/md0", "--level", "stripe", "raid-devices=2", "/dev/sda", "/dev/sdc" };
+    EXPECT_CALL(mdadm_process, execute(expected_args, _))
+        .WillOnce(Return(true));
+
+    MDAdmController controller(&mdadm_process);
+    controller.createRaid("/dev/md0", MDAdmController::Type::Raid0, QStringList({"/dev/sda", "/dev/sdc"}) );
 }
