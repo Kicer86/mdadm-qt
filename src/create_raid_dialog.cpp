ef17093170418b18608ca89095142cceeb821444
arko-pl <arek2407066@gmail.com>
2017-06-28 05:26:05 +0200

    Added more widgets to create RAID dialog. Added dummy function for RAID creation.

--- src/create_raid_dialog.cpp
+++ src/create_raid_dialog.cpp
@@ -3,43 +3,87 @@
 
 #include <QtDebug>
 
+#include <QFormLayout>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 
-#include <QPushButton>
+#include <QComboBox>
+#include <QLabel>
 #include <QListView>
+#include <QPushButton>
+#include <QSpinBox>
 
 CreateRaidDialog::CreateRaidDialog(QWidget *parent) :
     QDialog(parent),
     m_disksView(nullptr),
     m_selectedDisksView(nullptr),
     m_disksModel(),
-    m_selectedDisksModel()
+    m_selectedDisksModel(),
+    m_raidTypes({"RAID0", "RAID1", "RAID4", "RAID5", "RAID6", "RAID0+1"})
 {
+    QVBoxLayout *systemDisksLayout = new QVBoxLayout;
+    QVBoxLayout *buttonDiskLayout = new QVBoxLayout;
+    QVBoxLayout *selectedDisksLayout = new QVBoxLayout;
     QHBoxLayout *disksLayout = new QHBoxLayout;
-    QVBoxLayout *buttonLayout = new QVBoxLayout;
+    QFormLayout *optionsLayout = new QFormLayout;
+    QHBoxLayout *buttonCreateLayout = new QHBoxLayout;
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+
+    QLabel *labelDisks = new QLabel(tr("available disks:"));
+    QLabel *labelSelectedDisks = new QLabel(tr("disks for RAID:"));
 
     QPushButton *buttonAdd = new QPushButton(tr("->"));
     QPushButton *buttonRemove = new QPushButton(tr("<-"));
 
+    m_cbTypes = new QComboBox;
+    m_cbTypes->addItems(m_raidTypes);
+
+    m_sbDevNumber = new QSpinBox;
+
+    /* TODO: get ranges from other class */
+    m_sbDevNumber->setRange(0, 127);
+
+    QPushButton *buttonCancel = new QPushButton(tr("Cancel"));
+    QPushButton *buttonCreate = new QPushButton(tr("Create"));
+
     m_disksView = new QListView;
     m_selectedDisksView = new QListView;
 
-    buttonLayout->addStretch();
-    buttonLayout->addWidget(buttonAdd);
-    buttonLayout->addWidget(buttonRemove);
-    buttonLayout->addStretch();
+    systemDisksLayout->addWidget(labelDisks);
+    systemDisksLayout->addWidget(m_disksView);
 
-    disksLayout->addWidget(m_disksView);
-    disksLayout->addLayout(buttonLayout);
-    disksLayout->addWidget(m_selectedDisksView);
+    buttonDiskLayout->addStretch();
+    buttonDiskLayout->addWidget(buttonAdd);
+    buttonDiskLayout->addWidget(buttonRemove);
+    buttonDiskLayout->addStretch();
+
+    selectedDisksLayout->addWidget(labelSelectedDisks);
+    selectedDisksLayout->addWidget(m_selectedDisksView);
+
+    disksLayout->addLayout(systemDisksLayout);
+    disksLayout->addLayout(buttonDiskLayout);
+    disksLayout->addLayout(selectedDisksLayout);
+
+    optionsLayout->addRow(new QLabel("Type:"), m_cbTypes);
+    optionsLayout->setFieldGrowthPolicy(QFormLayout::FieldsStayAtSizeHint);
+    optionsLayout->addRow(new QLabel("MD device:"), m_sbDevNumber);
+
+    buttonCreateLayout->addStretch();
+    buttonCreateLayout->addWidget(buttonCancel);
+    buttonCreateLayout->addWidget(buttonCreate);
+
+    mainLayout->addLayout(disksLayout);
+    mainLayout->addLayout(optionsLayout);
+    mainLayout->addLayout(buttonCreateLayout);
 
     DiskController dc;
     auto disks = dc.listDisks();
 
     for (const auto& disk : disks) {
         QStandardItem* item = new QStandardItem(disk.toString());
-        item->data(0) = disk.name();
+        item->setData(disk.name());
         m_disksModel.appendRow(item);
     }
 
@@ -48,20 +92,31 @@ CreateRaidDialog::CreateRaidDialog(QWidget *parent) :
     connect(buttonAdd, &QPushButton::clicked, this, &CreateRaidDialog::addElements);
     connect(buttonRemove, &QPushButton::clicked, this, &CreateRaidDialog::removeElements);
 
+    connect(buttonCancel, &QPushButton::clicked, this, &CreateRaidDialog::close);
+    connect(buttonCreate, &QPushButton::clicked, this, &CreateRaidDialog::createRaid);
+
     m_disksView->setModel(&m_disksModel);
     m_selectedDisksView->setModel(&m_selectedDisksModel);
 
     m_disksView->setSelectionMode(QAbstractItemView::MultiSelection);
     m_selectedDisksView->setSelectionMode(QAbstractItemView::MultiSelection);
 
-    setLayout(disksLayout);
+    setLayout(mainLayout);
 }
 
 void CreateRaidDialog::addElements() {
     QItemSelectionModel *selectionModel = m_disksView->selectionModel();
+    std::vector<QPersistentModelIndex> rows_to_delete;
+
     for (const auto& elem : selectionModel->selectedIndexes()) {
+        if (!elem.isValid())
+            continue;
         QStandardItem* item = m_disksModel.takeItem(elem.row(), elem.column());
         m_selectedDisksModel.appendRow(item);
+        rows_to_delete.emplace_back(elem);
+    }
+    for (const auto& elem: rows_to_delete) {
+        m_disksModel.removeRow(elem.row());
     }
     m_disksModel.sort(0);
     m_selectedDisksModel.sort(0);
@@ -70,11 +125,35 @@ void CreateRaidDialog::addElements() {
 void CreateRaidDialog::removeElements() {
     QItemSelectionModel *selectionModel =
             m_selectedDisksView->selectionModel();
+    std::vector<QPersistentModelIndex> rows_to_delete;
+
     for (const auto& elem : selectionModel->selectedIndexes()) {
+        if (!elem.isValid())
+            continue;
+
         QStandardItem* item = m_selectedDisksModel
                 .takeItem(elem.row(), elem.column());
         m_disksModel.appendRow(item);
+        rows_to_delete.emplace_back(elem);
     }
+
+    for (const auto& elem : rows_to_delete) {
+        m_selectedDisksModel.removeRow(elem.row());
+    }
+
     m_selectedDisksModel.sort(0);
     m_disksModel.sort(0);
+}
+
+void CreateRaidDialog::createRaid() {
+    QStringList disks;
+
+    for (int row = 0; row < m_selectedDisksModel.rowCount(); ++row) {
+        QStandardItem *item = m_selectedDisksModel.item(row, 0);
+        if (item != nullptr)
+            disks.append(item->data().toString());
+    }
+    QString type = m_cbTypes->currentText();
+
+    qDebug() << disks << " " << type << " " << m_sbDevNumber->value();
 }
--- src/create_raid_dialog.hpp
+++ src/create_raid_dialog.hpp
@@ -3,18 +3,27 @@
 
 #include <QDialog>
 #include <QStandardItemModel>
+#include <QStringList>
 
 class QListView;
+class QComboBox;
+class QSpinBox;
 
 class CreateRaidDialog : public QDialog
-{
+{  
     QListView* m_disksView;
     QListView* m_selectedDisksView;
     QStandardItemModel m_disksModel;
     QStandardItemModel m_selectedDisksModel;
 
+    QComboBox *m_cbTypes;
+    QSpinBox *m_sbDevNumber;
+
+    const QStringList m_raidTypes;
+
     void addElements();
     void removeElements();
+    void createRaid();
 
 public:
     CreateRaidDialog(QWidget *parent = Q_NULLPTR);
