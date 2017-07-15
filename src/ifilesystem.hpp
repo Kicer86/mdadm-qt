5afb5c58ce98b1a587903586770e7a28f3148166
Michał Walenciak <Kicer86@gmail.com>
2017-07-15 09:06:19 +0200

    adding openFile() and listDir() to IFileSystem interface

--- src/ifilesystem.hpp
+++ src/ifilesystem.hpp
@@ -2,10 +2,25 @@
 #ifndef IFILESYSTEM_HPP
 #define IFILESYSTEM_HPP
 
+#include <deque>
+#include <memory>
+
+class QTextStream;
+class QString;
 
 struct IFileSystem
 {
+    struct IFile
+    {
+        virtual ~IFile() = default;
+
+        virtual QTextStream* getStream() = 0;
+    };
+
     virtual ~IFileSystem() = default;
+
+    virtual std::unique_ptr<IFile> openFile(const QString &) = 0;
+    virtual std::deque<QString> listDir(const QString &, const char* filter = "*.*") = 0;
 };
 
 #endif
