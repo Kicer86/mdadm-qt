44ed6de303e65af30174af80289c8394f8f73181
Michał Walenciak <Kicer86@gmail.com>
2017-07-23 20:01:57 +0200

    new test

--- src/disk_controller_tests.cpp
+++ src/disk_controller_tests.cpp
@@ -1,7 +1,14 @@
 
 #include <gmock/gmock.h>
 
+#include "disk.hpp"
 #include "disk_controller.hpp"
+#include "idisk_filter_mock.hpp"
+#include "ifilesystem_mock.hpp"
+
+
+using ::testing::_;
+using ::testing::Return;
 
 TEST(DiskControllerTest, isConstructible)
 {
@@ -9,4 +16,23 @@ TEST(DiskControllerTest, isConstructible)
     {
         DiskController dc(nullptr);
     });
+}
+
+
+TEST(DiskControllerTest, returnsAllDevicesWhenEmptyFilterIsBeingUsed)
+{
+    IDiskFilterMock filter;
+    IFileSystemMock fs;
+
+    const std::deque<QString> drives = { "sda", "sdb", "sdy", "sdz" };
+
+    EXPECT_CALL(filter, func_op(_)).WillRepeatedly(Return(true));
+    EXPECT_CALL(fs, listDir(QString("/sys/block"), _))
+        .WillOnce(Return(drives));
+
+    DiskController dc(&fs);
+
+    std::vector<std::unique_ptr<IBlockDevice>> devices = dc.listDisks(filter);
+
+    ASSERT_EQ(devices.size(), 4);
 }
