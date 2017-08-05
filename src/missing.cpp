5355961d695a3395c107ede25fa5f387ab417ef1
Arkadiusz Bubała <arek2407066@gmail.com>
2017-08-05 13:00:43 +0200

    Added "missing" device representation.

---
+++ src/missing.cpp
@@ -0,0 +1,56 @@
+/*
+ * The "missing" device representation.
+ * Copyright (C) 2017  Arkadiusz Bubała <arek2407066@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "missing.hpp"
+#include <QObject>
+
+Missing::Missing() : IBlockDevice()
+{
+
+}
+
+size_t Missing::size() const
+{
+    return 0;
+}
+
+size_t Missing::sizeInSectorUnits() const
+{
+    return 0;
+}
+
+unsigned Missing::logicalBlockSize() const
+{
+    return 0;
+}
+
+bool Missing::isUsed() const
+{
+    return false;
+}
+
+QString Missing::devPath() const
+{
+    return "missing";
+}
+
+QString Missing::toString() const
+{
+    return QObject::tr("Missing device representation");
+}
