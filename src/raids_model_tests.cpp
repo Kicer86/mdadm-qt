93bcd8bd639661412a5ce404d02de907bccea224
Michał Walenciak <Kicer86@gmail.com>
2017-09-30 16:16:32 +0200

    introducing first test

--- src/raids_model_tests.cpp
+++ src/raids_model_tests.cpp
@@ -1,1 +1,18 @@
 
+
+
+#include <gmock/gmock.h>
+
+#include "raids_model.hpp"
+
+
+TEST(RaidsModelTests, isEmptyWhenConstructed)
+{
+    RaidsModel model;
+
+    QAbstractItemModel* qt_model = model.model();
+
+    EXPECT_EQ(false, qt_model->canFetchMore(QModelIndex()));
+    EXPECT_EQ(0, qt_model->rowCount());
+    EXPECT_EQ(3, qt_model->columnCount());
+}
