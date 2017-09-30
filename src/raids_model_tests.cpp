5747cbe675f7fa80e3e345fe8829c959d1f9280c
Michał Walenciak <Kicer86@gmail.com>
2017-09-30 22:38:40 +0200

    adding test for appending

--- src/raids_model_tests.cpp
+++ src/raids_model_tests.cpp
@@ -140,9 +140,13 @@ struct ModelSignalWatcher: QObject
     {
         connect(model, &QAbstractItemModel::rowsRemoved,
                 this, &ModelSignalWatcher::rowsRemoved);
+
+        connect(model, &QAbstractItemModel::rowsInserted,
+                this, &ModelSignalWatcher::rowsInserted);
     }
 
     MOCK_METHOD3(rowsRemoved, void(const QModelIndex &parent, int first, int last));
+    MOCK_METHOD3(rowsInserted, void(const QModelIndex &parent, int first, int last));
 };
 
 
@@ -166,4 +170,34 @@ TEST_F(RaidsModelTests, removingRaidsFromModel)
 
     const std::vector<RaidInfo> raidsAfterChange = {raid1, raid3};
     model.load(raidsAfterChange);
+}
+
+
+TEST_F(RaidsModelTests, appendingRaidsToModel)
+{
+    const std::vector<RaidInfo> raids = {raid1,raid3};
+
+    RaidsModel model;
+    model.load(raids);
+
+    QAbstractItemModel* qt_model = model.model();
+
+    ModelSignalWatcher signalWatcher(qt_model);
+
+    // top item (raid2) will be added at the end (appended)
+    EXPECT_CALL(signalWatcher, rowsInserted(QModelIndex(), 2, 2));
+
+    // raid2's items cannot be observed here for 2 reasons:
+    // 1. current algorithm creates item for raid (but doesn't add it to the model yet),
+    //    then attaches components to it, and finally appends it to the model.
+    //    So model emits only one 'rowsInserted'.
+    // 2. We cannot ask model for QModelIndex of 'raid2' index, as
+    //    it doesn't exist in model yet.
+
+    const std::vector<RaidInfo> raidsAfterChange = {raid1, raid2, raid3};
+    model.load(raidsAfterChange);
+
+    // Make sure last item has 4 leafs
+    const QModelIndex raid2Idx = qt_model->index(2, 0);
+    EXPECT_EQ(4, qt_model->rowCount(raid2Idx));
 }
