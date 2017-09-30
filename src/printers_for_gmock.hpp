a6edc4ea22dd58b0f3035e0d7c0712d89a1fc543
Michał Walenciak <Kicer86@gmail.com>
2017-09-30 17:40:28 +0200

    adding QModelIndex to printers

--- src/printers_for_gmock.hpp
+++ src/printers_for_gmock.hpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <iostream>
 
+class QModelIndex;
 class QStringList;
 class QString;
 
@@ -14,5 +15,6 @@ void PrintTo(const QStringList& string_list, std::ostream* os);
 void PrintTo(const std::deque<QString>& string_deque, std::ostream* os);
 void PrintTo(const QString& str, std::ostream* os);
 void PrintTo(const RaidInfo& info, std::ostream* os);
+void PrintTo(const QModelIndex &, std::ostream* os);
 
 #endif
