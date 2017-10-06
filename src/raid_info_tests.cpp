931f82914d895b50e6b947356b534a1d3eccc4c9
Michał Walenciak <Kicer86@gmail.com>
2017-10-06 22:25:59 +0000

    adding tests for ComponentInfo's operator<()

--- src/raid_info_tests.cpp
+++ src/raid_info_tests.cpp
@@ -22,12 +22,28 @@ TEST_P(RaidInfoOperatorTest, operatorLess)
     EXPECT_EQ( false, raids.second < raids.first  );       // 2nd !< 1st
 }
 
+
+class RaidComponentInfoOperatorTest:
+    public testing::TestWithParam<std::pair<RaidComponentInfo, RaidComponentInfo>>
+{
+
+};
+
+
+TEST_P(RaidComponentInfoOperatorTest, operatorLess)
+{
+    const auto& components = GetParam();
+
+    EXPECT_EQ( true,  components.first  < components.second );     // 1st < 2nd
+    EXPECT_EQ( false, components.second < components.first  );     // 2nd !< 1st
+}
+
 namespace
 {
     const RaidComponentInfo dev1("dev1", RaidComponentInfo::Type::Normal, 128);
     const RaidComponentInfo dev2("dev2", RaidComponentInfo::Type::Normal, 129);
-    const RaidComponentInfo dev3("dev3", RaidComponentInfo::Type::Normal, 130);
-    const RaidComponentInfo dev4("dev4", RaidComponentInfo::Type::Normal, 131);
+    const RaidComponentInfo dev3("dev2", RaidComponentInfo::Type::Spare,  130);
+    const RaidComponentInfo dev4("dev2", RaidComponentInfo::Type::Normal, 131);
     const RaidComponentInfo dev5("dev5", RaidComponentInfo::Type::Normal, 132);
     const RaidComponentInfo dev6("dev6", RaidComponentInfo::Type::Normal, 133);
     const RaidComponentInfo dev7("dev7", RaidComponentInfo::Type::Normal, 134);
@@ -46,5 +62,15 @@ INSTANTIATE_TEST_CASE_P(RaidsSet,
                             std::make_pair(raid1, raid2),        // different device
                             std::make_pair(raid1, raid3),        // defferent type
                             std::make_pair(raid4, raid3)         // different components
+                        )
+);
+
+
+INSTANTIATE_TEST_CASE_P(RaidComponentsSet,
+                        RaidComponentInfoOperatorTest,
+                        ::testing::Values(
+                            std::make_pair(dev1, dev2),        // different device
+                            std::make_pair(dev2, dev3),        // defferent type
+                            std::make_pair(dev2, dev4)         // different descriptor
                         )
 );
